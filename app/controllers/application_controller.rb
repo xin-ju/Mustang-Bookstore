ef4cff643e290d39da2a24a21ef2bb370f23fa12
Chris Moore <csunidev@gmail.com>
2020-04-20 22:46:16 -0500

    New WIP iss12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+protect_from_forgery with: :exception
 
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+    before_action:configure_permitted_parameters, if: :devise_controller?
 
     protected
 
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :current_password) }
+       def configure_permitted_parameters
+           devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+           devise_parameter_sanitizer.permit(:account_update, keys: [:username]) 
         end
 end
